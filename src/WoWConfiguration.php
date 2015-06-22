a572dfa1f689d7381546ad97c5c358220cd72922
SAEZ Jonathan <saez.jonathan@gmail.com>
2015-06-22 23:41:01 +0200

    Configure mandatory information to work with wow apis

---
+++ src/WoWConfiguration.php
@@ -0,0 +1,38 @@
+<?php namespace GameScan\WoW;
+
+use GameScan\Core\Request\Api\ApiConfigurationInterface;
+use GameScan\Core\Tools\Environment;
+
+class WoWConfiguration implements ApiConfigurationInterface
+{
+    private  $apiKey = null;
+
+    /**
+     * Get parameters mandatory for request an api
+     * @return array
+     */
+    public function getParameters()
+    {
+        return [
+          "apikey"  => $this->getApiKey()
+        ];
+    }
+
+    /**
+     * Get headers mandatory for request an api
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return array();
+    }
+
+    private function getApiKey()
+    {
+        if($this->apiKey === null){
+            $this->apiKey =  (new Environment())->get("WOW_API_KEY");
+        }
+        return $this->apiKey;
+    }
+
+}
