32296136398da445f548f1c77f8345c8d71121c5
SAEZ Jonathan <saez.jonathan@gmail.com>
2015-07-18 20:48:39 +0200

    get player basics informations

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -23,4 +23,60 @@ class Player extends Base
     {
         return "wow/character/". $this->realmName ."/" . $this->characterName;
     }
+
+    public function getLastModified()
+    {
+        return $this->getEntityInformations()->lastInformation;
+    }
+
+    public function name()
+    {
+        return $this->getEntityInformations()->name;
+    }
+
+    public function realm()
+    {
+        return $this->getEntityInformations()->realm;
+    }
+
+    public function playerClass()
+    {
+        return $this->getEntityInformations()->class;
+    }
+
+    public function race()
+    {
+        return $this->getEntityInformations()->race;
+    }
+
+    public function gender()
+    {
+        return $this->getEntityInformations()->gender;
+    }
+
+    public function level()
+    {
+        return $this->getEntityInformations()->level;
+    }
+
+    public function achievementPoints()
+    {
+        return $this->getEntityInformations()->achievementPoints;
+    }
+
+
+    public function thumbnail()
+    {
+        return $this->getEntityInformations()->thumbnail;
+    }
+
+    public function calcClass()
+    {
+        return $this->getEntityInformations()->calcClass;
+    }
+
+    public function totalHonorableKills()
+    {
+        return $this->getEntityInformations()->totalHonorableKills;
+    }
 }
