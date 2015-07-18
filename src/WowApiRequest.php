defd7e3af996d9c0be59a3049850b989d7bdfe91
SAEZ Jonathan <saez.jonathan@gmail.com>
2015-07-18 19:02:48 +0200

    WoW api base + hostnotfoundException

--- src/WowApiRequest.php
+++ src/WowApiRequest.php
@@ -4,5 +4,17 @@ use GameScan\Core\Request\Api\GameApiRequest;
 
 class WowApiRequest extends GameApiRequest
 {
+    private $host = null;
 
+    public function setHost(HostInformations $hostInformations)
+    {
+        $this->host = $hostInformations;
+    }
+
+    public function setLocale()
+    {
+        if($this->host === null){
+            throw new
+        }
+    }
 }
